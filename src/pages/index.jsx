13f81e4f2aee68e1cef90f6d460fa62e17caf4d2
Anthony Clever <anthonyclever@gmail.com>
2019-04-10 20:22:24 -0400

    Randomizes image!

---
+++ src/pages/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+import Vampy from '../images/vampy.png';
+import VampyGif from '../images/vampygif.gif';
+import VampyGayOnion from '../images/vampygayonion.png';
+
+const images = [Vampy, VampyGif, VampyGayOnion];
+
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { random: 0 };
+  }
+
+  componentDidMount() {
+    this.setState(() => ({
+      random: Math.floor(Math.random() * 3),
+    }));
+
+    this.forceUpdate();
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO />
+        <h1>YES</h1>
+        <div id="vampy-container">
+          <img src={images[this.state.random]} alt="it's vampy!" />
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default IndexPage;
